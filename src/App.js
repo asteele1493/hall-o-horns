3c2a506b86fec38df80d98e0d8e2ee1a88d57d35
Andra <94204279+asteele1493@users.noreply.github.com>
2022-09-18 16:54:38 -0700

    Deleted files that were unnecessary and created files for lab's components.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
+//Capital react is the variable name. React in quotations is the library we're pulling from. 
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//This class is super important. 
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Ye Olde Hall o\' Horns </h1>
+      </div>
+    );
+  }
 }
 
 export default App;
