1717ce616023aa4bb5586bf5e78f8aa47d03d84f
Andra <94204279+asteele1493@users.noreply.github.com>
2022-09-22 22:01:21 -0700

    Added json data and refactored main page

--- src/HornedBeast.js
+++ src/HornedBeast.js
@@ -10,46 +10,38 @@ class HornedBeast extends React.Component {
     super(props);
     this.state = {
       favorites: 0
-    }
-    //   //We are not allowed to use this.state unless we refer to super and list our props. 
-    //   //State object will hold data about the class HornedBeast components
-    //   this.state = {
-    //     //You need an initial value of whatever state when the page gets loaded.
-    //     //Need a way to listen for that click to happen.
+    };
   }
-}
+
 
 addFavorites = () => {
-  this.setState({ favorites: this.state.favorites + 1 })
+  this.setState({ favorites: this.state.favorites + 1 });
+  };
 }
-//the placement for the event handler is super important. Between constructor and render method
-// handleClick = () => {
 
-// }
-
-//react classes require a render method. You need to call it like this.
 render() {
+
   return (
     <Container>
       <Col>
       <Card className = "h-100 p-2">
         <Card.Body>
-
-          <Card.Title>{this.props.title}</Card.Title>
-          <Card.Text>{this.props.description}</Card.Text>
-          <Card.Img variant="top"
+        <Card.Img variant="top"
             src={this.props.imageURL}
             alt={this.props.description}
             title={this.props.title}
             onClick={this.addFavorites} />
+          <Card.Title>{this.props.title}</Card.Title>
+          <Card.Text>{this.props.description}</Card.Text>
           <Card.Text>{this.props.favorites}</Card.Text>
 
         </Card.Body>
       </Card>
       </Col>
     </Container>
-  )
+  );
 }
+
 
 
 
